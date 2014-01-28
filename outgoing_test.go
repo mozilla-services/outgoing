331e86ecf4e71358e9fb7628a92fcb17380fa70b
Jeremiah Orem <oremj@oremj.com>
2014-01-28 10:10:08 -0800

    Add javascript: test.

--- outgoing_test.go
+++ outgoing_test.go
@@ -1,12 +1,12 @@
 package main
 
 import (
+	"crypto/hmac"
 	"crypto/sha1"
 	"crypto/sha256"
-	"crypto/hmac"
 	"fmt"
-	"net/http/httptest"
 	"net/http"
+	"net/http/httptest"
 	"strings"
 	"testing"
 )
@@ -19,7 +19,7 @@ func init() {
 func TestValidateSig(t *testing.T) {
 	url := "http://mozilla.org/en-US/"
 	s1 := sha1.New()
-	s1.Write([]byte("secret"+ url))
+	s1.Write([]byte("secret" + url))
 	sig1 := fmt.Sprintf("%x", s1.Sum(nil))
 
 	if !validateSig(url, sig1) {
@@ -27,37 +27,37 @@ func TestValidateSig(t *testing.T) {
 	}
 
 	s1 = sha1.New()
-	s1.Write([]byte("badsecret"+ url))
+	s1.Write([]byte("badsecret" + url))
 	sig1 = fmt.Sprintf("%x", s1.Sum(nil))
 
 	if validateSig(url, sig1) {
 		t.Errorf("bad url hash: %s matched.", sig1)
 	}
 
-	h := hmac.New(sha256.New, []byte("secret"))
-	h.Write([]byte(url))
-	hSig := fmt.Sprintf("%x", h.Sum(nil))
+	hSig := getHmac(url, "secret")
 
 	if !validateSig(url, hSig) {
 		t.Errorf("url hash: %s did not match.", hSig)
 	}
 
-	h = hmac.New(sha256.New, []byte("badsecret"))
-	h.Write([]byte(url))
-	hSig = fmt.Sprintf("%x", h.Sum(nil))
+	hSig = getHmac(url, "badsecret")
 
 	if validateSig(url, hSig) {
-		t.Errorf("bad url hash: %s did not match.", hSig)
+		t.Errorf("bad url hash: %s matched.", hSig)
 	}
 }
 
+func getHmac(url, key string) string {
+	h := hmac.New(sha256.New, []byte(key))
+	h.Write([]byte(url))
+	return fmt.Sprintf("%x", h.Sum(nil))
+}
+
 func TestReq(t *testing.T) {
-	h := hmac.New(sha256.New, []byte("secret"))
-	h.Write([]byte("http://www.mywot.com/"))
-	hSig := fmt.Sprintf("%x", h.Sum(nil))
+	hSig := getHmac("http://www.mywot.com/", "secret")
 
 	rec := httptest.NewRecorder()
-	req, err := http.NewRequest("GET", "/v1/" + hSig + "/http%3A//www.mywot.com/", nil)
+	req, err := http.NewRequest("GET", "/v1/"+hSig+"/http%3A//www.mywot.com/", nil)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -69,17 +69,27 @@ func TestReq(t *testing.T) {
 		t.Errorf("http://www.mywot.com/ is not in %s.", rec.Body.String())
 	}
 
-	h = hmac.New(sha256.New, []byte("badsecret"))
-	h.Write([]byte("http://www.mywot.com/"))
-	hSig = fmt.Sprintf("%x", h.Sum(nil))
+	hSig = getHmac("http://www.mywot.com/", "badsecret")
 
 	rec = httptest.NewRecorder()
-	req, err = http.NewRequest("GET", "/v1/" + hSig + "/http%3A//www.mywot.com/", nil)
+	req, err = http.NewRequest("GET", "/v1/"+hSig+"/http%3A//www.mywot.com/", nil)
 	if err != nil {
 		t.Fatal(err)
 	}
 	readReq(rec, req)
 	if rec.Code != 400 {
-		t.Errorf("Expected %d, returned %d.", 200, rec.Code)
+		t.Errorf("Expected %d, returned %d.", 400, rec.Code)
+	}
+
+	hSig = getHmac("javascript:alert()", "badsecret")
+
+	rec = httptest.NewRecorder()
+	req, err = http.NewRequest("GET", "/v1/"+hSig+"/javascript:alert()", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	readReq(rec, req)
+	if rec.Code != 400 {
+		t.Errorf("Expected %d, returned %d.", 400, rec.Code)
 	}
 }
