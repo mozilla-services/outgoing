72a8b7662d2a67da131bde1f9637e997d682b5bb
Jeremiah Orem <oremj@oremj.com>
2016-09-27 14:26:18 -0700

    add more test cases

--- outgoing_test.go
+++ outgoing_test.go
@@ -55,60 +55,52 @@ func getHmac(url, key string) string {
 }
 
 func TestReq(t *testing.T) {
-	// Test URL no query string
-	hSig := getHmac("http://www.mozilla.org/", "secret")
+	testCases := []struct {
+		URL         string
+		RequestPath string
+		Expected    string
+	}{
+		{"http://www.mozilla.org/", "http%3A//www.mozilla.org/", `href="http://www.mozilla.org/"`},                                                                                                                                                                                              // Plain URL
+		{"http://www.mozilla.org/?foo=bar&boo=baz", "http%3A//www.mozilla.org/%3Ffoo=bar&boo=baz", `href="http://www.mozilla.org/?foo=bar&amp;boo=baz`},                                                                                                                                         // URL with query params
+		{`http://www.mozilla.org/"><script>alert()</script>`, `http%3A//www.mozilla.org/%22%3E%3Cscript%3Ealert%28%29%3C/script%3E%0A`, `<a href="http://www.mozilla.org/%22%3e%3cscript%3ealert%28%29%3c/script%3e">http://www.mozilla.org/&#34;&gt;&lt;script&gt;alert()&lt;/script&gt;</a>`}, // URL with crazy characters
+	}
+	for _, tc := range testCases {
+		hSig := getHmac(tc.URL, "secret")
 
-	rec := httptest.NewRecorder()
-	req, err := http.NewRequest("GET", "/v1/"+hSig+"/http%3A//www.mozilla.org/", nil)
-	if err != nil {
-		t.Fatalf("hmac: %s err: %v", hSig, err)
-	}
-	readReq(rec, req)
-	if rec.Code != 200 {
-		t.Errorf("Expected %d, returned %d. hmac: %s", 200, rec.Code, hSig)
-	}
-	if !strings.Contains(rec.Body.String(), `href="http://www.mozilla.org/"`) {
-		t.Errorf("http://www.mozilla.org/ is not in %s.", rec.Body.String())
-	}
+		rec := httptest.NewRecorder()
+		req, err := http.NewRequest("GET", "/v1/"+hSig+"/"+tc.RequestPath, nil)
+		if err != nil {
+			t.Fatalf("hmac: %s err: %v", hSig, err)
+		}
+		readReq(rec, req)
+		if rec.Code != 200 {
+			t.Errorf("Expected %d, returned %d. hmac: %s", 200, rec.Code, hSig)
+		}
+		if !strings.Contains(rec.Body.String(), tc.Expected) {
+			t.Errorf("%s is not in %s.", tc.Expected, rec.Body.String())
+		}
 
-	// Test urls with & character
-	hSig = getHmac("http://www.mozilla.org/?foo=bar&boo=baz", "secret")
-
-	rec = httptest.NewRecorder()
-	req, err = http.NewRequest("GET", "/v1/"+hSig+"/http%3A//www.mozilla.org/%3Ffoo=bar&boo=baz", nil)
-	if err != nil {
-		t.Fatalf("hmac: %s err: %v", hSig, err)
-	}
-	readReq(rec, req)
-	if rec.Code != 200 {
-		t.Errorf("Expected %d, returned %d. hmac: %s", 200, rec.Code, hSig)
-	}
-	if !strings.Contains(rec.Body.String(), `href="http://www.mozilla.org/?foo=bar&amp;boo=baz"`) {
-		t.Errorf("http://www.mozilla.org/?foo=bar&amp;boo=baz is not in %s.", rec.Body.String())
 	}
 
-	// Test bad secret
-	hSig = getHmac("http://www.mozilla.org/", "badsecret")
-
-	rec = httptest.NewRecorder()
-	req, err = http.NewRequest("GET", "/v1/"+hSig+"/http%3A//www.mozilla.org/", nil)
-	if err != nil {
-		t.Fatal(err)
-	}
-	readReq(rec, req)
-	if rec.Code != 400 {
-		t.Errorf("Expected %d, returned %d.", 400, rec.Code)
+	failCases := []struct {
+		URL         string
+		RequestPath string
+		Secret      string
+	}{
+		{"http://www.mozilla.org/", "http%3A//www.mozilla.org/", "badsecret"},
+		{"javascript:alert()", "javascript:alert()", "secret"},
 	}
 
-	hSig = getHmac("javascript:alert()", "badsecret")
-
-	rec = httptest.NewRecorder()
-	req, err = http.NewRequest("GET", "/v1/"+hSig+"/javascript:alert()", nil)
-	if err != nil {
-		t.Fatal(err)
-	}
-	readReq(rec, req)
-	if rec.Code != 400 {
-		t.Errorf("Expected %d, returned %d.", 400, rec.Code)
+	for _, tc := range failCases {
+		hSig := getHmac(tc.URL, tc.Secret)
+		rec := httptest.NewRecorder()
+		req, err := http.NewRequest("GET", "/v1/"+hSig+"/"+tc.RequestPath, nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+		readReq(rec, req)
+		if rec.Code != 400 {
+			t.Errorf("Expected %d, returned %d.", 400, rec.Code)
+		}
 	}
 }
